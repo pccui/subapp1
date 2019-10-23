3d682de2d21e849aad26964a0e42c4439000b7c3
p.cui <p.cui@mobizcorp.de>
2019-10-23 11:41:49 +0200

    remove conficts

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const cherry = "cherry";
 // cherry2
 const cherry2 = "cherry2";
 // bug fix1
-<<<<<<< HEAD
-cosnt bugfix = "01";
-=======
 const bugfix = "01";
->>>>>>> bugfix1
 // bug fix2
 const bugfix2 = "01";
